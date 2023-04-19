7728dde42143cc118501f6238c59c108e3ca0260
PualKim93 <kj_93@kakao.com>
2023-04-19 21:53:21 +0900

    feat: eslint, prettier 설정

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,13 +6,13 @@ module.exports = {
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
+  extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
   root: true,
   env: {
-    node: true,
+    //node: true,
+    //jest: true,
+    browser: true,
+    es2021: true,
     jest: true,
   },
   ignorePatterns: ['.eslintrc.js'],
@@ -21,5 +21,14 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-inferrable-types': 'error',
+    //indent: [4, 'tab'],
+    'no-tabs': 0,
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
   },
 };
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,55 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule, SwaggerCustomOptions } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
+import { express } from 'express-useragent';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.use(express());
+  const appPort: number = (app.get(ConfigService).get('API_PORT') as number) || 3000;
+
+  //CORS 활성
+  app.enableCors({
+    origin: true,
+    methods: 'GET,PUT,PATCH,POST,DELETE',
+    credentials: false,
+  });
+  app.useStaticAssets(join(__dirname, '../dist'));
+
+  //유효성 검사 PIPE
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  if (process.env.NODE_ENV === 'development') {
+    //swagger
+    const swaggerConfig = new DocumentBuilder()
+      .setTitle(`NEST JS API`)
+      .setDescription('NEST JS API Authorize')
+      .setVersion('1.1')
+      .addBearerAuth({ type: 'http', scheme: 'bearer', bearerFormat: 'Bearer' }, 'access-token')
+      .build();
+
+    const swaggerOption: SwaggerCustomOptions = {
+      swaggerOptions: {
+        persistAuthorization: true,
+        defaultModelsExpandDepth: -1,
+      },
+      customSiteTitle: 'NEST JS API [NEST JS]',
+    };
+    const document = SwaggerModule.createDocument(app, swaggerConfig, { deepScanRoutes: true });
+    SwaggerModule.setup('api-docs', app, document, swaggerOption);
+  }
+
+  await app.listen(appPort, '0.0.0.0');
+  console.log(`app start port : ${appPort}`);
 }
 bootstrap();
