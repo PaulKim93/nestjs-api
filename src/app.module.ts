cc45c85fc039663996e97f6cb562a04f763ed30f
PualKim93 <kj_93@kakao.com>
2023-04-20 11:45:20 +0900

    feat: config vaildation 체크 Joi 적용

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,28 @@ import { AllExceptionsFilter } from '@app/share/modules/filter/all-exception.fil
 import { ConfigModule } from '@nestjs/config';
 import { APP_FILTER } from '@nestjs/core';
 import { mysqlConection } from '@app/share/modules/db/mysql-connection';
-
+import * as Joi from 'joi';
 @Module({
   imports: [
     //config 설정
     ConfigModule.forRoot({
       envFilePath: `.${process.env.NODE_ENV || ''}.env`,
       isGlobal: true,
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().valid('development', 'production', 'stage').required(),
+        API_PORT: Joi.number().default(3000),
+        DATABASE_TYPE: Joi.string(),
+        DATABASE_HOST: Joi.string(),
+        DATABASE_USER: Joi.string(),
+        DATABASE_PASSWORD: Joi.string(),
+        DATABASE_PORT: Joi.string(),
+        DATABASE_NAME: Joi.string(),
+        JWT_SECRET: Joi.string(),
+        JWT_TTL: Joi.number().default(60),
+        JWT_REFRESH_TTL: Joi.number().default(600),
+        JWT_ALGO: Joi.string(),
+        JWT_JTI: Joi.string(),
+      }),
     }),
     mysqlConection,
   ],
